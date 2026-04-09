e8f793d882368643afd82d8230228f07e1704d89
Yasmin Guimarães <yas2019.guima@gmail.com>
2026-04-09 01:05:51 -0300

    Dash bem legal
    dash dash dash dash dash

--- Assets/scripts actions/DashAction.cs
+++ Assets/scripts actions/DashAction.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 public class DashAction : ActionModel
 {
+    public float dashForce = 20f, dashCooldown = 1f, dashCount = 0f, dashTime = 0.2f, MoveSpeed;
+    private bool isDashing = false, canDash = true;
     public override void Use()
     {
+        if (!CanUseAction()) return;
         
+        if (canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+        float ogMoveSpeed = MoveSpeed;
+        MoveSpeed = MoveSpeed * dashForce;
+
+        yield return WaitForSeconds(dashCooldown);
+        canDash = true;
+        isDashing = false;
     }
 }
