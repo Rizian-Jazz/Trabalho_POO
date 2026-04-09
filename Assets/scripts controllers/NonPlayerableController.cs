2196521a9635446bd4c733ed0103750e4c6d5640
Marcolino Anctônio B.C.T. <boaperguntadasilva@gmail.com>
2026-04-09 02:55:22 -0300

    Nonjogaveljogado
    meti bronca nele ai dentro

--- Assets/scripts controllers/NonPlayerableController.cs
+++ Assets/scripts controllers/NonPlayerableController.cs
@@ -1,17 +1,52 @@
-using System.Collections.Specialized;
 using UnityEngine;
-
+using System.Colletions; 
 public class NonPlayerableController : BaseCharacterController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float intervalo = 6.7f;
+    private bool Choraxx = false;//
+
+
+    // sim
     void Start()
     {
-    
+        StartCoroutine(Buzanfa());
+    }
+    private IEnumerator Buzanfa()
+    {
+        while (true)
+        {
+            if (!Choraxx)
+            {
+              yield return StartCoroutine(Atumalaca());
+            }
+            
+            yield return new WaitForSeconds(intervalo);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Atumalaca()
     {
-        
+        Choraxx = true; //como?
+        int acoes = Random.Range(0, 4);
+        switch(acoes)
+        {
+            case 0:
+                Jump();
+                yield return new WaitForSeconds(1f);
+                break;
+            case 1:
+                DoubleJump();
+                yield return new WaitForSeconds(1f);
+                break;
+            case 2:
+                SquareWalk();
+                yield return new WaitForSeconds(1f);
+                break;
+            case 3:
+                Dash();
+                yield return new WaitForSeconds(1f);
+                break;
+        }
+        Choraxx = false;
     }
 }
